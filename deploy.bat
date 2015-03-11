603063a4a0e31080e6a102d0caf5ddcd2a41348a
lucifer <lucifer@ixiaopu.com>
2015-03-11 11:18:01 +0800

    deploy

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 rem set MAVEN_OPTS=-Dfile.encoding=UTF-8
-call mvn -U -Denv=dev -DfailIfNoTests=false -P dev clean install package
+call mvn -U -Denv=dev -DfailIfNoTests=false -P dev clean install package assembly:assembly
 
 echo ------------------------------
 
