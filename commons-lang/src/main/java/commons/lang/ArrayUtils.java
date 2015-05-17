43f7157f0bdbf878961241e75f060767a4742399
lucifer <lucifer@ixiaopu.com>
2015-05-17 20:29:51 +0800

    增加plus方法，数组求和

--- commons-lang/src/main/java/commons/lang/ArrayUtils.java
+++ commons-lang/src/main/java/commons/lang/ArrayUtils.java
@@ -8,11 +8,20 @@ package commons.lang;
  * @since 10:22:04 AM Nov 25, 2013
  */
 public class ArrayUtils {
-
+	/**
+	 * @see #toByteObjectArray(String[])
+	 * @param arr
+	 * @return
+	 */
 	public static byte[] toByteArray(String[] arr) {
 		return org.apache.commons.lang3.ArrayUtils.toPrimitive(toByteObjectArray(arr));
 	}
 
+	/**
+	 * 数组转换，转换成Byte[]，调用{@link Byte#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static Byte[] toByteObjectArray(String[] arr) {
 		if (arr == null) {
 			return null;
@@ -28,10 +37,20 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * @see #toShortObjectArray(String[])
+	 * @param arr
+	 * @return
+	 */
 	public static short[] toShortArray(String[] arr) {
 		return org.apache.commons.lang3.ArrayUtils.toPrimitive(toShortObjectArray(arr));
 	}
 
+	/**
+	 * 数组转换，转换成Short[]，调用{@link Short#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static Short[] toShortObjectArray(String[] arr) {
 		if (arr == null) {
 			return null;
@@ -47,10 +66,20 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * @see #toIntegerObjectArray(String[])
+	 * @param arr
+	 * @return
+	 */
 	public static int[] toIntArray(String[] arr) {
 		return org.apache.commons.lang3.ArrayUtils.toPrimitive(toIntegerObjectArray(arr));
 	}
 
+	/**
+	 * 数组转换，转换成Integer[]，调用{@link Integer#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static Integer[] toIntegerObjectArray(String[] arr) {
 		if (arr == null) {
 			return null;
@@ -66,10 +95,20 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * @see #toLongObjectArray(String[])
+	 * @param arr
+	 * @return
+	 */
 	public static long[] toLongArray(String[] arr) {
 		return org.apache.commons.lang3.ArrayUtils.toPrimitive(toLongObjectArray(arr));
 	}
 
+	/**
+	 * 数组转换，转换成Long[]，调用{@link Long#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static Long[] toLongObjectArray(String[] arr) {
 		if (arr == null) {
 			return null;
@@ -85,10 +124,20 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * @see #toFloatObjectArray(String[])
+	 * @param arr
+	 * @return
+	 */
 	public static float[] toFloatArray(String[] arr) {
 		return org.apache.commons.lang3.ArrayUtils.toPrimitive(toFloatObjectArray(arr));
 	}
 
+	/**
+	 * 数组转换，转换成Float[]，调用{@link Float#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static Float[] toFloatObjectArray(String[] arr) {
 		if (arr == null) {
 			return null;
@@ -104,10 +153,20 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * @see #toDoubleObjectArray(String[])
+	 * @param arr
+	 * @return
+	 */
 	public static double[] toDoubleArray(String[] arr) {
 		return org.apache.commons.lang3.ArrayUtils.toPrimitive(toDoubleObjectArray(arr));
 	}
 
+	/**
+	 * 数组转换，转换成Double[]，调用{@link Double#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static Double[] toDoubleObjectArray(String[] arr) {
 		if (arr == null) {
 			return null;
@@ -123,10 +182,20 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * @see #toCharacterObjectArray(String[])
+	 * @param arr
+	 * @return
+	 */
 	public static char[] toCharArray(String[] arr) {
 		return org.apache.commons.lang3.ArrayUtils.toPrimitive(toCharacterObjectArray(arr));
 	}
 
+	/**
+	 * 数组转换，转换成Character[]，调用{@link Character#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static Character[] toCharacterObjectArray(String[] arr) {
 		if (arr == null) {
 			return null;
@@ -142,6 +211,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link String#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(byte[] arr) {
 		if (arr == null) {
 			return null;
@@ -157,6 +231,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link Object#toString()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(Byte[] arr) {
 		if (arr == null) {
 			return null;
@@ -172,6 +251,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link String#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(short[] arr) {
 		if (arr == null) {
 			return null;
@@ -187,6 +271,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link Object#toString()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(Short[] arr) {
 		if (arr == null) {
 			return null;
@@ -202,6 +291,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link String#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(int[] arr) {
 		if (arr == null) {
 			return null;
@@ -217,6 +311,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link Object#toString()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(Integer[] arr) {
 		if (arr == null) {
 			return null;
@@ -232,6 +331,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link String#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(long[] arr) {
 		if (arr == null) {
 			return null;
@@ -247,6 +351,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link Object#toString()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(Long[] arr) {
 		if (arr == null) {
 			return null;
@@ -262,6 +371,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link String#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(float[] arr) {
 		if (arr == null) {
 			return null;
@@ -277,6 +391,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link Object#toString()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(Float[] arr) {
 		if (arr == null) {
 			return null;
@@ -292,6 +411,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link String#valueOf()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(double[] arr) {
 		if (arr == null) {
 			return null;
@@ -307,6 +431,11 @@ public class ArrayUtils {
 		return results;
 	}
 
+	/**
+	 * 数组转换，转换成String[]，调用{@link Object#toString()}
+	 * @param arr
+	 * @return
+	 */
 	public static String[] toStringArray(Double[] arr) {
 		if (arr == null) {
 			return null;
@@ -320,5 +449,167 @@ public class ArrayUtils {
 			results[i] = arr[i].toString();
 		}
 		return results;
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static long plus(byte[] arr) {
+		long result = 0;
+		for (int e : arr) {
+			result += e;
+		}
+		return result;
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static long plus(Byte[] arr) {
+		return plusForLong(arr);
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static long plus(short[] arr) {
+		long result = 0;
+		for (short e : arr) {
+			result += e;
+		}
+		return result;
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static long plus(Short[] arr) {
+		return plusForLong(arr);
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static long plus(int[] arr) {
+		long result = 0;
+		for (int e : arr) {
+			result += e;
+		}
+		return result;
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static long plus(Integer[] arr) {
+		return plusForLong(arr);
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static long plus(long[] arr) {
+		long result = 0;
+		for (long e : arr) {
+			result += e;
+		}
+		return result;
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static long plus(Long[] arr) {
+		return plusForLong(arr);
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static long plusForLong(Number[] arr) {
+		long result = 0;
+		for (Number e : arr) {
+			if (e != null) {
+				result += e.longValue();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static double plus(float[] arr) {
+		double result = 0;
+		for (float e : arr) {
+			result += e;
+		}
+		return result;
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static double plus(Float[] arr) {
+		return plusForDouble(arr);
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static double plus(double[] arr) {
+		double result = 0;
+		for (double e : arr) {
+			result += e;
+		}
+		return result;
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static double plus(Double[] arr) {
+		return plusForDouble(arr);
+	}
+
+	/**
+	 * 数组求和
+	 * @param arr
+	 * @return
+	 */
+	public static double plusForDouble(Number[] arr) {
+		double result = 0;
+		for (Number e : arr) {
+			if (e != null) {
+				result += e.doubleValue();
+			}
+		}
+		return result;
 	}
 }
